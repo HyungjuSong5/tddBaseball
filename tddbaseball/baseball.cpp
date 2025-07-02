c9cf7f01c50b7784d77adac926e63ddcc6e3b9aa
Hyungju Song <hyungju.song54@gmail.com>
2025-07-02 13:14:12 +0900

    [feature] 30 page

--- tddbaseball/baseball.cpp
+++ tddbaseball/baseball.cpp
@@ -9,5 +9,11 @@ class Baseball {
     if (string.length() != 3) {
       throw length_error("Must be three letter.");
     }
+
+    for (char ch : string) {
+      if (ch < '0' || ch > '9') {
+        throw invalid_argument("Must be number");
+      }
+    }
   }
 };
--- tddbaseball/baseball_test.cpp
+++ tddbaseball/baseball_test.cpp
@@ -9,6 +9,11 @@ TEST(BASEBALLGame, ThrowExceptionWhenInputLengthIsUnmatched) {
   EXPECT_THROW(game.guess(string("12")), length_error);
 }
 
+TEST(BASEBALLGame, ThrowExceptionWhenInvalidChar) {
+  Baseball game;
+  EXPECT_THROW(game.guess(string("12s")), invalid_argument);
+}
+
 int main() {
   ::testing::InitGoogleMock();
   return RUN_ALL_TESTS();
