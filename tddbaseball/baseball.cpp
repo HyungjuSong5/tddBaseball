41bdd538ab00beca3ed7f14b94a67f1219e61f5e
Hyungju Song <hyungju.song54@gmail.com>
2025-07-02 13:32:06 +0900

    [feature] 49 page

--- tddbaseball/baseball.cpp
+++ tddbaseball/baseball.cpp
@@ -3,10 +3,20 @@
 
 using namespace std;
 
+struct GuessResult {
+  bool solved;
+  int strikes;
+  int balls;
+};
+
 class Baseball {
  public:
-  void guess(const string& guessNumber) { 
+  explicit Baseball(const string& question)
+      : question(question) {}
+
+  GuessResult guess(const string& guessNumber) {
       assertIllegalArgument(guessNumber);
+    return {true, 3, 0};
   }
 
   void assertIllegalArgument(const std::string& guessNumber) {
@@ -29,4 +39,7 @@ class Baseball {
             guessNumber[1] == guessNumber[2] ||
             guessNumber[0] == guessNumber[2]);
   }
+
+ private:
+  string question;
 };
